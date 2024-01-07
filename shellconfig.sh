fde5e276f0f77271c4d236cfea21d6af55a3fde5
e811-py <69768793+e811-py@users.noreply.github.com>
2024-01-07 20:39:52 +0330

    Update shellconfig.sh

--- shellconfig.sh
+++ shellconfig.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+ZSH_CUSTOM=~/.oh-my-zsh
+
 # Detect package manager
 if [ -x "$(command -v apt-get)" ]; then
     PM="apt-get"
@@ -22,6 +24,7 @@ else
     exit 1
 fi
 
+
 # Install Zsh
 sudo $PM $UC
 if ! [ -x "$(command -v zsh)" ]; then
@@ -31,36 +34,53 @@ else
 fi
 
 
+# Install neofech
+if ! [ -x "$(command -v neofetch)" ]; then
+    sudo $PM $IC -y neofetch
+else
+    echo -e "\u001b[33m[\u001b[31m!\u001b[33m]\u001b[31m neofetch is already installed."
+fi
+
+
 # Install Oh-my-zsh
-if ! [ -d ~/.oh-my-zsh ]; then
+if ! [ -e ~/.oh-my-zsh ]; then
     echo exit | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # echo out=$($ZSH_CUSTOM) && echo $out && exit | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
     echo -e "\u001b[33m[\u001b[31m!\u001b[33m]\u001b[31m Oh-my-zsh is already installed."
 fi
 
 
 # Install zsh-autosuggestions
-if ! [ -d $ZSH_CUSTOM/plugins/zsh-autosuggestions ]; then
+if ! [ -e $ZSH_CUSTOM/plugins/zsh-autosuggestions ]; then
     sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 else
     echo -e "\u001b[33m[\u001b[31m!\u001b[33m]\u001b[31m Zsh-autosuggestions is already installed."
 fi
 
 # Install zsh-syntax-highlighting
-if ! [ -d $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]; then
+if ! [ -e $ZSH_CUSTOM/plugins/zsh-syntax-highlighting ]; then
     sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 else
     echo -e "\u001b[33m[\u001b[31m!\u001b[33m]\u001b[31m Zsh-syntax-highlighting is already installed."
 fi
 
+
 # Update .zshrc file
-if [ -f ~/.zshrc ]; then
+if [ -e ~/.zshrc ]; then
     sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
     sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/g' ~/.zshrc
+    if ! grep -Fxq "neofetch" ~/.zshrc;then
+        echo "neofetch" >> ~/.zshrc
+    fi
     echo -e "\u001b[33m[\u001b[32m!\u001b[33m]\u001b[32m Plugins configed in .zshrc"
 else
-    echo 'plugins=(git zsh-autosuggestions zsh-syntax-highlighting)' > ~/.zshrc
+    echo 'ZSH_THEME="bira"\nplugins=(git zsh-autosuggestions zsh-syntax-highlighting)\nneofetch' > ~/.zshrc
+    echo -e "\u001b[33m[\u001b[32m!\u001b[33m]\u001b[32m A new .zshrc created."
 fi
 
 # Set Zsh as default shell
 sudo chsh -s $(which zsh)
+
+# Go to zsh :)
+zsh
