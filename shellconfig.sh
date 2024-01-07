01abaae7b8789da3f5f4bd78d6868a57ce85bb2e
e811-py <69768793+e811-py@users.noreply.github.com>
2024-01-07 12:20:39 +0330

    Update shellconfig.sh

--- shellconfig.sh
+++ shellconfig.sh
@@ -32,10 +32,10 @@ fi
 
 
 # Install zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # Install zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
 # Update .zshrc file
 if [ -f "~/.zshrc" ]; then
